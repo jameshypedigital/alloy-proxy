ee44b2c4f94367d73d391eedc4736200ebb9d799
jameshypedigital <james@hypedigital.ai>
2026-02-02 08:48:42 -0600

    Create index.js

---
+++ pages/index.js
@@ -0,0 +1,13 @@
+export default function Home() {
+  return (
+    <div style={{
+      height: "100vh",
+      display: "flex",
+      alignItems: "center",
+      justifyContent: "center",
+      fontFamily: "system-ui"
+    }}>
+      <h2>Alloy Redirect Proxy is live 🚀</h2>
+    </div>
+  );
+}
